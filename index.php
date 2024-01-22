3e6cfbfc48ea3a3f4c5fe94f84437092d71b3d95
Hertz <106058099+yuhertz@users.noreply.github.com>
2024-01-22 12:07:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+
 <?php
 	try{
 		if (!file_exists('anti_ddos/start.php'))
@@ -8,7 +9,7 @@
 	//CATCH the exception if something goes wrong.
 	catch (Exception $ex) {
 		echo '<div style="padding:10px;color:white;position:fixed;top:0;left:0;width:100%;background:black;text-align:center;">'.
-			'The <a href="https://github.com/sanix-darker/antiddos-system" target="_blank">"AntiDDOS System"</a> failed to load '.
+			'The <a href="https://github.com/yuhertz/cybersecurity" target="_blank">"AntiDDOS System"</a> failed to load '.
 			'properly on this Web Site, please de-comment the \'catch Exception\' to see what happening!</div>';
 		//Print out the exception message.
 		//echo $ex->getMessage();
@@ -24,9 +25,7 @@
 	<body>
 		<center>
 			<div class="container">
-				<a href="https://github.com/Sanix-Darker">
-					<img src="https://avatars1.githubusercontent.com/u/22576758?s=400&v=4" style="border-radius: 100%;width:200px;"><br>
-					Sanix Darker Github
+				<a href="https://github.com/yuhertz">
 				</a>
 				<h1>Example Webpage</h1>
 				<p>This web Site is protected by AntiDDOS-System PHP.</p>
