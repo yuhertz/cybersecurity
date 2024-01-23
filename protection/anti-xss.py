28b9de9557ad16681d237d690ab7eb77c9887b26
Hertz <106058099+yuhertz@users.noreply.github.com>
2024-01-23 09:00:30 +0800

    Create anti-xss.py

---
+++ protection/anti-xss.py
@@ -0,0 +1,16 @@
+import bleach
+
+def sanitize_input(user_input):
+    # Allow only certain tags and attributes for safety
+    allowed_tags = ['a', 'em', 'strong', 'b', 'i', 'u', 'p', 'br', 'ol', 'ul', 'li']
+    allowed_attrs = {'a': ['href']}
+
+    # Sanitize user input
+    sanitized_input = bleach.clean(user_input, tags=allowed_tags, attributes=allowed_attrs)
+
+    return sanitized_input
+
+# Test the function
+user_input = "<script>alert('XSS Attack!');</script>"
+sanitized_input = sanitize_input(user_input)
+print(sanitized_input)
